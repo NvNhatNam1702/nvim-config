97f1d5a28173062bc8c13428c0f0f2a33689c500
nampc <nvnhatnam1234@gmail.com>
2025-11-29 21:32:23 +0700

    add keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+--select all the file
+map("n", "<C-a>", "ggVG", { desc = "Select all" })
+map("i", "<C-a>", "<Esc>ggVG", { desc = "Select all" })
